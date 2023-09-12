9a7cb1eef0651fe96b4a946d60f4c7000ad03878
PrzemekMiros <kontakt@futurewebstudio.pl>
2023-09-12 12:42:37 +0200

    Swiper lazy load img

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -33,14 +33,8 @@ function animationMain() {
       var swiper = new Swiper(".swiper", {
         grabCursor: true,
         spaceBetween: 20,
+        lazy: true,
         centeredSlides: false,
-        coverflowEffect: {
-          rotate: 0,
-          stretch: 0,
-          depth: 0,
-          modifier: 1,
-          slideShadows: false
-        },
         loop: true,
         pagination: {
           el: ".swiper-pagination",
